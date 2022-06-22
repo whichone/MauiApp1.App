3461b987c4dd57a408a5f6cfb7b86e7ef3de67dd
whichone <hhcnau@gmail.com>
2022-06-22 22:14:56 +0930

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		Shell.Current.GoToAsync("//Annotation");
+	}
+}
+
---
+++ Views/CalloutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1.Views;
+
+public partial class CalloutPage : ContentPage
+{
+	public CalloutPage()
+	{
+		InitializeComponent();
+	}
+}
